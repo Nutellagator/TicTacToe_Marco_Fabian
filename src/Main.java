ddc09b3bc086ec110bdae14cbe1d7f82f4ab5785
Marco <pimientapfoehler@gmail.com>
2023-09-13 11:54:54 +0200

    sout test

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,10 @@ public class Main {
         System.out.println("test1");System.out.println("test1");
         System.out.println("test1");
         System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");System.out.println("test1");
+        System.out.println("test1");
+        System.out.println("test1");
 
     }
 
