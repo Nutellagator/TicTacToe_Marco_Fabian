ceed95be90f8a4b548e85bece696f67e2f35d43f
Marco <pimientapfoehler@gmail.com>
2023-09-14 12:08:43 +0200

    updateSpielbrett Methode

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
         for(char[] row : spielBrett){  // (for-each) for- Schleife für Array's...for (type variableName : arrayName)
             for( char[]Zeile : spielBrett){
-                for(char c : row){              // Erst Zeile, dann Spalte
+                for(char c : row){              // Erst Zeile, dann Spalte = 2D-char-Array
                     System.out.println(c);
                 }
                 System.out.println();
@@ -18,7 +18,15 @@ public class Main {
         }
     }
 
+    public static void updateSpielbrett(int position, int spieler, char [][] spielBrett){
 
+        char character;
+        if(spieler == 1){
+            character = 'X';            // ' '  statt " " weil char Datentyp
+        } else {
+            character = 'O';
+        }
+    }
 
 
 
