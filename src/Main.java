304f3da5b0a4699e614f243368997680ced38855
Fabian <FabianHoehler@hotmail.com>
2023-09-14 11:43:54 +0200

    Anwendung von for-each

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+    // Multidimensionales-Array, Darstellung des Spielfeldes
+    char [][] Spielbrett = {{'_','|','_','|','_','|'},{'_','|','_','|','_','|'},{'_','|','_','|','_','|'}};
 
+    }
+    public static void printBrett(char [][] Spielbrett){
 
+        for(char[] row : Spielbrett){  // (for-each) for- Schleife für Array's...for (type variableName : arrayName)
+
+        }
     }
 
 }
--- src/Notizen.java
+++ src/Notizen.java
@@ -15,11 +15,21 @@ oder
 _ | _ | _
 _ | _ | _
   |   |
-
-Mehrdimensionales Array - Idee
+________________________________________________________________________________________________________________________
+Multidimensionales Array - Idee
 
 [0][0] , [0][2] , [0][4]
 [1][0] , [1][2] , [1][4]
 [2][0] , [2][2] , [2][4]
+________________________________________________________________________________________________________________________
+Beispiel For-each loop:
+
+Im folgenden Beispiel werden alle Elemente im Array „cars “ mithilfe einer „ for-each “-Schleife ausgegeben:
+
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+    for (String i : cars) {
+    System.out.println(i);
+________________________________________________________________________________________________________________________
+}
 
  */
